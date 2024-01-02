657c844749695e6df28f557a230ceca0dffdd516
BlackKoneko <hdicmsr@naver.com>
2024-01-02 19:49:12 +0900

    싱글톤, 게임매니저, UI매니저, 사운드매니저 추가

---
+++ Assets/Script/Pattern/Singleton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = (T)this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
